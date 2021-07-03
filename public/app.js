7c5aaaf070c4e3a4a635c2a9f5c3cdfc5d3201a7
katiepdx <61799774+katiepdx@users.noreply.github.com>
2021-07-02 18:25:14 -0700

    add socketio - counter

---
+++ public/app.js
@@ -0,0 +1,11 @@
+// const button = document.getElementById('button');
+// // io coming in through the cdn in script tag
+// const socket = io();
+
+// button.addEventListener('click', () => {
+//   socket.emit('user-click', Date.now());
+// });
+
+// socket.on('increment-click', (counter) => {
+//   button.textContent = counter;
+// });
--- server.js
+++ server.js
@@ -1,14 +1,42 @@
 import app from './lib/app.js';
-import pool from './lib/utils/pool.js';
 
-const PORT = process.env.PORT || 7890;
-
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Started on ${PORT}`);
+// SERVER SETUP
+// wrap app in an http server
+import http from 'http';
+const server = http.createServer(app);
+// wrap the http server with a socketio server
+import { Server } from 'socket.io';
+// add cors - https://socket.io/docs/v4/handling-cors/
+const io = new Server(server, { 
+  cors: {
+    origin: 'http://localhost:3000',
+    methods: ['GET'],
+    credentials: true
+  }
 });
 
-process.on('exit', () => {
-  console.log('Goodbye!');
-  pool.end();
+// SERVER PORT
+const PORT = process.env.PORT || 7890;
+
+let counter = 0;
+
+// SOCKET IO 
+io.on('connection', (socket) => {
+  console.log('a user connected', socket.id);
+
+  // broadcast current count to all connected clients
+  socket.broadcast.emit('current-count', counter);
+  
+  // when client clicks btn
+  socket.on('user-click', () => {
+    // server sends info back to client with updated count
+    io.emit('increment-click', counter++);
+
+    console.log(`user ${socket.id} clicked ${counter} times`);
+  });
+  
+});
+
+server.listen(PORT, () => {
+  console.log(`listening on ${PORT}`);
 });
